0974a3bb1d897a15a66cad556b63ad6659ca76f3
donyoun1 <mjimhdon@gmail.com>
2012-01-25 14:19:26 -0700

    Initial commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,26 @@
 class UsersController < ApplicationController
+  # GET /users
+  # GET /users.xml
+  def index
+    @users = User.all
 
+    respond_to do |format|
+      format.html # index.html.erb
+      format.xml  { render :xml => @users }
+    end
+  end
+
+
+   def create
+    @user = User.new(params[:user])
+    if @user.save
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
+    else
+      @title = "Sign up"
+      render 'new'
+    end
+  end
 
   def show
     @user = User.find(params[:id])
