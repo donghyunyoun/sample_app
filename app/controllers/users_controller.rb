90d7630c652ad23d104bcf025c0e30cb501a9e1a
donyoun1 <mjimhdon@gmail.com>
2012-01-25 10:42:13 -0700

    User model with passwords, and user show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,12 @@
 class UsersController < ApplicationController
-   def new
+
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def new
+    @user = User.new
     @title = "Sign up"
   end
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120118171357) do
+ActiveRecord::Schema.define(:version => 20120125172038) do
 
   create_table "microposts", :force => true do |t|
     t.string   "content"
@@ -25,6 +25,10 @@ ActiveRecord::Schema.define(:version => 20120118171357) do
     t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "salt"
+    t.string   "encrypted_password"
   end
+
+  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
 
 end
