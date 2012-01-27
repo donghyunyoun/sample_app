6f0590118539364e6111f764cbf654af88edd1c0
donghyunyoun <dhyoun10@gmail.com>
2012-01-27 16:15:38 -0700

    Done with with sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,21 @@
 SampleApp::Application.routes.draw do
 
 
+  get "sessions/new"
+
   match '/contact', :to => 'pages#contact'
   match '/about',   :to => 'pages#about'
   match '/help',    :to => 'pages#help'
   match '/signup',   :to => 'users#new'
+  match '/signin',    :to => 'sessions#new'
+  match '/signout',     :to => 'sessions#destroy'
   root :to => 'pages#home'
 
   resources :microposts
 
   resources :users
+
+  resources :sessions, :only => [:new, :create, :destory]
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
